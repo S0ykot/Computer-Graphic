be751e4f08280244b2b362990d3eb2e128005682
Md Sameull Soykot <soykot.ch@gmail.com>
2020-01-27 10:51:34 +0600

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -9,10 +9,18 @@ void display()
 	glClearColor(0.0,0.0,0.0,0.0);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glBegin(GL_TRIANGLES);
-	glColor3f(1.0, 1.0, 0.0);
-	glVertex2f(.5,0.0);
-	glVertex2f(.5,.5);
-	glVertex2f(0.0,0.0);
+	//glColor3f(1.0, 0.0, 0.0);
+	glColor3ub(255,255,255);
+	glVertex2f(0.5,0.0);
+	glVertex2f(0.0,.5);
+	glVertex2f(-0.5,0.0);
+	glEnd();
+	glBegin(GL_QUADS);
+	glColor3ub(218,47,47);
+	glVertex2f(.4,-.4);
+	glVertex2f(.4,0.0);
+	glVertex2f(-.4,0);
+	glVertex2f(-.4,-.4);
 	glEnd();
 
 	glFlush();
