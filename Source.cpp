8c0f48351e7b721ad793d9a620b70b46f1279b3f
Md Sameull Soykot <soykot.ch@gmail.com>
2020-02-03 01:32:10 +0600

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -6,21 +6,54 @@ using namespace std;
 
 void display()
 {
-	glClearColor(0.0,0.0,0.0,0.0);
+	glClearColor(1,1,1,1);
 	glClear(GL_COLOR_BUFFER_BIT);
+	//glBegin(GL_TRIANGLES);
+	////glColor3f(1.0, 0.0, 0.0);
+	//glColor3ub(43,67,11);
+	//glVertex2f(0.5,0.0);
+	//glVertex2f(0.0,.5);
+	//glVertex2f(-0.5,0.0);
+	//glEnd();
+
 	glBegin(GL_TRIANGLES);
-	//glColor3f(1.0, 0.0, 0.0);
-	glColor3ub(255,255,255);
-	glVertex2f(0.5,0.0);
-	glVertex2f(0.0,.5);
-	glVertex2f(-0.5,0.0);
+	glColor3ub(43,67,11);
+	glVertex2f(.5,0);
+	glVertex2f(0,.5);
+	glVertex2f(-.5,0);
 	glEnd();
+
+
 	glBegin(GL_QUADS);
 	glColor3ub(218,47,47);
+	glVertex2f(.5,-.5);
+	glVertex2f(.5,0);
+	glVertex2f(-.5,0);
+	glVertex2f(-.5,-.5);
+	glEnd();
+
+	glBegin(GL_QUADS);
+	glColor3ub(50, 168, 82);
 	glVertex2f(.4,-.4);
-	glVertex2f(.4,0.0);
-	glVertex2f(-.4,0);
+	glVertex2f(.4,-.2);
+	glVertex2f(.2,-.2);
+	glVertex2f(.2,-.4);
+	glEnd();
+
+	glBegin(GL_QUADS);
+	glColor3ub(50, 168, 82);
 	glVertex2f(-.4,-.4);
+	glVertex2f(-.4,-.2);
+	glVertex2f(-.2,-.2);
+	glVertex2f(-.2,-.4);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glColor3ub(1, 1, 1);
+	glVertex2f(.6,-.6);
+	glVertex2f(.5,-.5);
+	glVertex2f(-.5,-.5);
+	glVertex2f(-.6,-.6);
 	glEnd();
 
 	glFlush();
